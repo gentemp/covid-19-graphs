713ba0d3db2cc468ea2257c1501abba0c117014e
Johan Lindberg <johan@expan.se>
2020-03-23 10:15:56 +0100

    Updated the lancet graph with correct date format and a script to batch updates.

--- lancet1.py
+++ lancet1.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-if __name__ == "__main__":
+from datetime import datetime as dt
+
+def run():
     confirmed = data.get_total_time_series('confirmed')
     deaths = data.get_total_time_series('deaths')
 
@@ -21,7 +23,7 @@ if __name__ == "__main__":
     for i in range(len(deaths)):
         j = i - 14
         if j >= 0:
-            dates.append(deaths.index[i])
+            dates.append(dt.strftime(dt.strptime(deaths.index[i], "%m/%d/%y"), "%b %d"))
             ratio.append(float(deaths[i]) / confirmed[j] * 100)
 
     fig, ax = plt.subplots(figsize = [12, 5])
@@ -33,4 +35,10 @@ if __name__ == "__main__":
     ax.set_xlabel('WHO report date')
     ax.set_xticks(dates[::5])
 
-    plt.savefig(data.img_path + 'Lancet1-Figure1-new.png')
+    today = dt.strftime(dt.today(), "%Y-%m-%d") 
+    plt.savefig(data.img_path + 'Lancet1-Figure1-%s.png' % (today))
+    plt.savefig(data.img_path + 'Lancet1-Figure1-latest.png')
+
+if __name__ == "__main__":
+    run()
+    plt.show()
---
+++ update_all.py
@@ -0,0 +1,6 @@
+## run all scripts to update all graphs
+
+import lancet1
+
+if __name__ == "__main__":
+    lancet1.run()
