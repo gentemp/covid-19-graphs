46d172cc24316e09b9adba0a9c9138a4d1f6d3ed
Johan Lindberg <johan@expan.se>
2020-03-29 21:17:55 +0200

    Added a polyfit prediction to the graphs

--- mj4.py
+++ mj4.py
@@ -48,12 +48,16 @@ def rolling_average(values, dates, n):
 
     return result, dates[3:-1]
 
+def fit_a_line(values, deg = 2):
+    x = np.linspace(0, len(values), len(values))
+    return np.poly1d(np.polyfit(x, values, deg))
+
 def run_for(close, country):
     df = data.get_country_time_series('deaths', country)
 
     # get the timeseries of cumulative confirmed deaths
     # starting on the day of the first death
-    values, dates = align_data(df, n = 1, d = '3/10/20')
+    values, dates = align_data(df, n = 1)
 
     # calculate the rolling average of the confirmed deaths
     _values, _ = rolling_average(values, dates, n = 4)
@@ -66,6 +70,15 @@ def run_for(close, country):
 
     fig, ax = plt.subplots(figsize = [14, 5])
     ax.plot(dates, _rows, color = 'red', marker ='o')
+    
+    p1 = fit_a_line(_rows, deg = 1)
+    p2 = fit_a_line(_rows, deg = 2)
+    p3 = fit_a_line(_rows, deg = 3)
+    t = np.linspace(0, len(_rows))
+    ax.plot(t, p1(t), color = '#ff0000', linestyle ='--')
+    ax.plot(t, p2(t), color = '#a00000', linestyle ='--')
+    ax.plot(t, p3(t), color = '#800000', linestyle ='--')
+
     ax.grid(True)
 
     ax.set_title(country)
@@ -95,5 +108,5 @@ def run(close = True, countries = None):
         run_for(close, country)
 
 if __name__ == "__main__":
-    run(close = False, countries = ['Italy'])
+    run(close = False)
     plt.show()
